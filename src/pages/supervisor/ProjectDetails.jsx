f71419721a4cc8e5d03559aecbb8ccb5f67aefdf
Anushtha <kumarsen.bash@gmail.com>
2023-11-29 00:35:44 +0530

    request a project and get list of pending request

--- src/pages/supervisor/ProjectDetails.jsx
+++ src/pages/supervisor/ProjectDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useReadCypher, useLazyWriteCypher } from "use-neo4j";
 
@@ -20,8 +20,31 @@ const user_query = `MATCH (project:Project {project_id: $project_id})<-[:ASSIGNE
 RETURN user
 `;
 
+const request_project_query = `MATCH (user:User {user_id: $user_id})
+MATCH (project:Project {project_id: $project_id})
+MERGE (user)-[:REQUEST_TO]->(project)
+RETURN user, project
+`;
+
+const has_user_requested_query = `MATCH (user:User {user_id: $user_id})-[:REQUEST_TO]->(project:Project {project_id: $project_id})
+RETURN COUNT(user) > 0 AS hasRequested
+`;
+
+const requested_users_query = `
+MATCH (user:User)-[:REQUEST_TO]->(project:Project {project_id: $project_id})
+RETURN user
+`;
+
+const accept_request_query = `MATCH (user:User {user_id: 'your_user_id'})-[request:REQUEST_TO]->(project:Project {project_id: 'your_project_id'})
+DELETE request
+CREATE (user)-[:ASSIGNED_TO]->(project)
+RETURN user, project
+`;
+
 const ProjectDetails = () => {
 	const { projectid } = useParams();
+
+	const [isRequesting, setisRequesting] = useState(false);
 
 	const user = useAuth();
 
@@ -33,11 +56,25 @@ const ProjectDetails = () => {
 		project_id: projectid,
 	});
 
+	const is_user_has_requested_q_res = useReadCypher(has_user_requested_query, {
+		project_id: projectid,
+		user_id: user.user_id,
+	});
+
+	const requested_users_res = useReadCypher(requested_users_query, {
+		project_id: projectid,
+	});
+
 	const [assign_user, assign_query_state] = useLazyWriteCypher(assign_query);
+	const [request_to_join, request_query_state] = useLazyWriteCypher(
+		request_project_query
+	);
 
 	let createdByDetails = {};
 	let projectDetails = {};
 	let assigned_users = [];
+	let pending_users = [];
+	let is_request_pending = false;
 
 	if (fetch_assign_users.records) {
 		assigned_users = fetch_assign_users.records.map(
@@ -47,10 +84,26 @@ const ProjectDetails = () => {
 		assigned_users = [];
 	}
 
+	if (is_user_has_requested_q_res.first) {
+		is_request_pending = is_user_has_requested_q_res.first.get("hasRequested");
+	}
+
+	if (requested_users_res.records) {
+		pending_users = requested_users_res.records.map(
+			(row) => row.get("user").properties
+		);
+	}
+
+	let is_user_assigned =
+		assigned_users.findIndex((v) => v.user_id === user.user_id) > -1;
+	let is_candidate = user.role_id === 1;
+
 	if (first) {
 		projectDetails = first.get("project").properties;
 		createdByDetails = first.get("user").properties;
 	}
+
+	let is_user_is_creator = createdByDetails.user_id === user.user_id;
 
 	const handleSelect = (user) => {
 		assign_user({ user_id: user.user_id, project_id: projectid })
@@ -60,6 +113,24 @@ const ProjectDetails = () => {
 			})
 			.catch(() => {
 				alert("Failed to assign a user!");
+			});
+	};
+
+	const handleRequest = () => {
+		setisRequesting(true);
+		request_to_join({ user_id: user.user_id, project_id: projectid })
+			.then(() => {
+				alert("Request has been sent!");
+				is_user_has_requested_q_res.run({
+					project_id: projectid,
+					user_id: user.user_id,
+				});
+			})
+			.catch(() => {
+				alert("Requesting has been failed!");
+			})
+			.finally((e) => {
+				setisRequesting(false);
 			});
 	};
 
@@ -92,7 +163,7 @@ const ProjectDetails = () => {
 						</p>
 					</div>
 
-					<div class="bg-white p-8 rounded shadow">
+					<div class="bg-white p-8 rounded shadow flex flex-col gap-2">
 						<div className="flex justify-between items-center">
 							<h2 class="text-2xl font-bold mb-4">Assigned Candidates</h2>
 
@@ -128,7 +199,39 @@ const ProjectDetails = () => {
 								</li>
 							))}
 						</ul>
+						{!is_user_assigned && is_candidate ? (
+							<button
+								onClick={handleRequest}
+								class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+								disabled={is_request_pending || isRequesting}>
+								{isRequesting
+									? "Loading..."
+									: is_request_pending
+									? "Pending"
+									: "Request to join"}
+							</button>
+						) : null}
 					</div>
+
+					{is_user_is_creator ? (
+						<div class="bg-white p-8 rounded shadow flex flex-col gap-2">
+							<h2 class="text-2xl font-bold mb-4">Pending Requets</h2>
+							{pending_users.map((v) => (
+								<li key={v.user_id} class="flex items-center space-x-4">
+									<img
+										src="https://via.placeholder.com/50"
+										alt="User Avatar"
+										class="w-10 h-10 rounded-full"
+									/>
+									<div>
+										<h3 class="text-md font-semibold capitalize">
+											{v.full_name}
+										</h3>
+									</div>
+								</li>
+							))}
+						</div>
+					) : null}
 				</div>
 			</div>
 		</Layout>
